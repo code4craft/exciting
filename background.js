ebf8c8f4c2570625335e1d8ec084b896392e44df
yihua.huang <code4crafter@gmail.com>
2013-07-02 22:47:13 +0800

    update

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 repo="code4craft/blackhole"
-timeIntervel=300000
+timeIntervel=30000
 function check(){
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function()
@@ -12,15 +12,15 @@ function check(){
     	var oldStars=localStorage["stars"];
     	var oldFolks=localStorage["forks"];
     	if (obj.watchers_count>oldStars || obj.forks_count>oldFolks){
-    	
-    	localStorage["stars"]=obj.watchers_count;
-    	localStorage["forks"]=obj.forks_count;
     	var notification = webkitNotifications.createNotification(
   'https://secure.gravatar.com/avatar/4ce9123a05ae222d71d2857316cbe699?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',  // icon url - can be relative
   'Github new star!',  // notification title
   'New star at '+repo+'! Now at '+obj.watchers_count+' stars and '+obj.forks_count + ' forks'  // notification body text
 );
-    	notification.onclick = function(x) { chrome.tabs.create({
+    	notification.onclick = function(x) { 
+		localStorage["stars"]=obj.watchers_count;
+    	localStorage["forks"]=obj.forks_count;
+    		chrome.tabs.create({
     		url:"https://github.com/"+repo
     	});this.cancel(); };
 notification.show();
