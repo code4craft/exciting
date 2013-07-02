df10dcddf2b73c39be57539e72e69d85c46dd71f
yihua.huang <code4crafter@gmail.com>
2013-07-02 22:44:56 +0800

    set interval

--- background.js
+++ background.js
@@ -16,8 +16,8 @@ function check(){
     	localStorage["stars"]=obj.watchers_count;
     	localStorage["forks"]=obj.forks_count;
     	var notification = webkitNotifications.createNotification(
-  'fluidicon.png',  // icon url - can be relative
-  'Github exciting!',  // notification title
+  'https://secure.gravatar.com/avatar/4ce9123a05ae222d71d2857316cbe699?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',  // icon url - can be relative
+  'Github new star!',  // notification title
   'New star at '+repo+'! Now at '+obj.watchers_count+' stars and '+obj.forks_count + ' forks'  // notification body text
 );
     	notification.onclick = function(x) { chrome.tabs.create({
@@ -31,4 +31,4 @@ notification.show();
 xmlhttp.open("GET","https://api.github.com/repos/"+repo,true);
 xmlhttp.send();
 }
-setInterval(check(),timeIntervel)
+setInterval(check,timeIntervel)
